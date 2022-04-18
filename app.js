05ed64ae3788649bc7b1396200da08238158b663
Agustin Arriaga <63561976+agustinishere@users.noreply.github.com>
2022-04-17 23:31:51 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+//Global selections and variables
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+const popup = document.querySelector(".copy-container");
