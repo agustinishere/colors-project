60394c4e2275d617f304fe511173d03128887552
Agustin Arriaga <63561976+agustinishere@users.noreply.github.com>
2022-04-26 22:12:13 -0700

    adding checkTextContrast

--- app.js
+++ app.js
@@ -92,3 +92,12 @@ function randomColors() {
     checkTextContrast(initialColors[index], lockButton[index]);
   });
 }
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
+}
