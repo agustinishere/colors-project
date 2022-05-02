f42d93b0f02dd01981f04fdd1800df6d855768a5
Agustin Arriaga <63561976+agustinishere@users.noreply.github.com>
2022-05-01 21:02:10 -0700

    adding copyToClipboard function

--- app.js
+++ app.js
@@ -176,3 +176,15 @@ function resetInputs() {
     }
   });
 }
+function copyToClipboard(hex) {
+  const el = document.createElement("textarea");
+  el.value = hex.innerText;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+  //Pop up animation
+  const popupBox = popup.children[0];
+  popup.classList.add("active");
+  popupBox.classList.add("active");
+}
